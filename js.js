7ede18b37fa2bba23af266ed25a255ae3f619599
HenKat13 <hennesseykatie13@gmail.com>
2019-05-12 21:15:13 -0400

    moment and append

--- js.js
+++ js.js
@@ -60,6 +60,36 @@ $("#new-train").on("click", function (event) {
     var nextTrain = moment().add(tMinutesTillTrain, "minutes");
     console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
 
+    dataRef.ref().on("child_added", function(childSnapshot) {
+
+        // Log everything that's coming out of snapshot
+        console.log(childSnapshot.val().name);
+        console.log(childSnapshot.val().name);
+        console.log(childSnapshot.val().email);
+        console.log(childSnapshot.val().age);
+        console.log(childSnapshot.val().comment);
+        console.log(childSnapshot.val().joinDate);
+  
+        // full list of items to the well
+        $("#full-member-list").append("<div class='well'><span class='member-name'> " +
+          childSnapshot.val().name +
+          " </span><span class='member-email'> " + childSnapshot.val().email +
+          " </span><span class='member-age'> " + childSnapshot.val().age +
+          " </span><span class='member-comment'> " + childSnapshot.val().comment +
+          " </span></div>");
+  
+        // Handle the errors
+      }, function(errorObject) {
+        console.log("Errors handled: " + errorObject.code);
+      });
+  
+      dataRef.ref().orderByChild("dateAdded").limitToLast(1).on("child_added", function(snapshot) {
+        // Change the HTML to reflect
+        $("#name-display").text(snapshot.val().name);
+        $("#email-display").text(snapshot.val().email);
+        $("#age-display").text(snapshot.val().age);
+        $("#comment-display").text(snapshot.val().comment);
+      });   
 });
 // create a listener for database: database.on(childadded)
 // trainname destination, frequency, first arrival from the snapshot
@@ -68,4 +98,3 @@ $("#new-train").on("click", function (event) {
 // for each of the variables create a table data (td)
 // append all (td) to table row (tr) 
 // append (tr) to (tbody)
-// activity 19 has addchild and 21 train predictions
